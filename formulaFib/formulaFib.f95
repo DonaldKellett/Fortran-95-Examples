32c0979dc2c2e68759fa90c530422fcb97874fcc
DonaldKellett <i.donaldl@me.com>
2017-12-12 14:05:57 +0800

    Add formula Fibonacci implementation

---
+++ formulaFib/formulaFib.f95
@@ -0,0 +1,11 @@
+module FormulaFib
+  implicit none
+  real(kind=8), parameter, private :: phi = 1.618033988749894848204586834365638117720309179805762862135D0
+  contains
+    ! NOTE: Only works for small `n` (upto and including n = 46);
+    ! otherwise, interger overflow occurs
+    function fibonacci(n) result(retval)
+      integer :: n, retval
+      retval = nint((phi ** n - (-phi) ** -n) / (2 * phi - 1))
+    end function fibonacci
+end module FormulaFib
